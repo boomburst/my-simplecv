947bcabce2df5a1c5008104157b1310b6ac0e8b3
Balaje <balaje6@gmail.com>
2014-03-15 21:07:06 +0530

    digit recognition

--- digit/digit.py
+++ digit/digit.py
@@ -1,35 +1,67 @@
-#Simple script to detect digits using basic geometry (8, 6, 9)
-#Use an image file as argument 
+#crude program to recognize digit without ml 
 
 from SimpleCV import *
 from sys import *
 
+def findDigit(blobs, corners, lines):
+    
+    if(len(blobs) == 3):
+        return 8
+#--------------------------------------
+    elif(len(blobs) == 2):
+        if(len(lines) >= 10):
+            return 4
+
+        for i in range(0, len(blobs)-1):
+            if(i != len(blobs)-1):
+                center = blobs[i].centroid()
+                center1 = blobs[i+1].centroid()
+                
+                if(center[1] < center1[1]):
+                    return 9
+                elif(center[1] > center1[1]):
+                    return 6
+#------------------------------------
+    elif(len(blobs) == 1):
+        if(len(corners) >= 7 and len(lines) == 2):
+            return 1
+        elif(len(lines) == 3 and len(corners) == 7):
+            return -1
+        elif(len(corners) == 6):
+            return 2
+        elif(len(corners) == 9 and len(lines) == 7):
+            return 5
+
+        return 0
+#------------------------------------
 script, digit = argv
 
 display = Display()
-img = Image(digit)
+image = Image(digit)
 
+digit = 0
 while display.isNotDone():
-    blobs = img.findBlobs().sortArea()
+   
+    blobs = image.findBlobs()
+    corners = image.findCorners()
+    lines = image.findLines()
+        
     if blobs is not None:
-        for i in range(0, len(blobs)-1):
-            blobs[i].draw(Color.GREEN, width =-1)
-            if(len(blobs) == 3):
-                print "The number might be a 8"
-                exit()
-            
-            if(len(blobs) == 2):
-                if(i != len(blobs)-1):
-                    center = blobs[i].centroid()
-                    center1 = blobs[i+1].centroid()
+        blobs.draw(Color.GREEN, width=4)
+    if corners is not None:
+        corners.draw(Color.RED, width=4)
+    if lines is not None:
+        lines.draw(Color.BLUE, width=4)
+        
+    digit = findDigit(blobs, corners, lines)
 
-                    if(center[1] < center1[1]):
-                        print "The number might be a 9"
-                        exit()
-
-                    if(center[1] > center1[1]):
-                        print "The number might be a 6"            
-                        exit()
-                            
-print len(blobs)
+    image.show()        
+                        
+print "No of blobs: ", len(blobs)
+print "No of corners: ", len(corners)
+print "No of lines: ", len(lines)
+if(digit == -1):
+    print "Possible digit: 7 or 3 "
+else:
+    print "Possible digit: ",digit
                 
